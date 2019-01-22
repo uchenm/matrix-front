0827ffd6391e959f1f09c41e0a5ae80a40bb0fb6
zhaoxueyu <312364705@qq.com>
2019-01-22 16:43:35 +0800

    头部widget

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,120 @@
 
 import React, { PureComponent } from 'react'
+import { Icon, Popover } from 'antd'
 import Link from 'umi/link'
 import styles from './index.less'
 
+/* 通知 */
+const Notification_Items = [
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+  {
+    title: 'New Bitcoin and Altcoin Quarterly Futures Contracts',
+    time: '12月13日 下午12:29',
+    content: 'On 17th December 2018, the March 2019 quarterly ADA, BCH, EOS, ETH, LTC, TRX, and XRP futures contracts will be listed:'
+  },
+]
+const Notification = () => (
+  <div className={styles.notification}>
+    <div className={styles.headerWidgetTitle}>公告和更新</div>
+    <div className={styles.content}>
+      {
+        Notification_Items.map((item, index) => (
+          <div className={styles.item} key={index}>
+            <div className={styles.head}>
+              <div className={`f-16 c-darkblue`}>{item.title}</div>
+              <div className={`c-000-7`}>{item.time}</div>
+            </div>
+            <div className={`f-12`}>{item.content}</div>
+          </div>
+        ))
+      }
+    </div>
+  </div>
+)
+/* 语言 */
+const Lang = () => (
+  <div className={styles.lang}>
+    <div className={styles.headerWidgetTitle}>语言</div>
+    <div className={styles.content}>
+      <div className={`flex flex-justify-between align-items-center cursor-p ${styles.item}`}>
+        <span>English</span>
+        <img className={styles.countryIcon} src="/image/en-US.svg" alt=""/>
+      </div>
+      <div className={`flex flex-justify-between align-items-center cursor-p ${styles.item}`}>
+        <span>中文</span>
+        <img className={styles.countryIcon} src="/image/zh-CN.svg" alt=""/>
+      </div>
+      <div className={`flex flex-justify-between align-items-center cursor-p ${styles.item}`}>
+        <span>Русский</span>
+        <img className={styles.countryIcon} src="/image/ru-RU.svg" alt=""/>
+      </div>
+      <div className={`flex flex-justify-between align-items-center cursor-p ${styles.item}`}>
+        <span>한국어</span>
+        <img className={styles.countryIcon} src="/image/ko-KR.svg" alt=""/>
+      </div>
+      <div className={`flex flex-justify-between align-items-center cursor-p ${styles.item}`}>
+        <span>日本語</span>
+        <img className={styles.countryIcon} src="/image/ja-JP.svg" alt=""/>
+      </div>
+    </div>
+  </div>
+)
+/* 账户 */
+const Account = () => (
+  <div className={styles.account}>
+    <div className={styles.headerWidgetTitle}>主要选项</div>
+    <div className={styles.content}>
+      <div className={styles.btnGroup}>
+        <div className={`${styles.accountBtn} cursor-p bg-light-blue flex align-items-center`}>
+          <Icon type="user" style={{color: '#fff', fontSize: '18px', margin: '0 15px'}}/>
+          <span className={`flex-1 f-bolder t-center c-fff ${styles.txt}`}>账户和安全性</span>
+        </div>
+        <div className={`${styles.accountBtn} cursor-p bg-light-blue flex align-items-center`}>
+          <Icon type="setting" style={{color: '#fff', fontSize: '18px', margin: '0 15px'}}/>
+          <span className={`flex-1 f-bolder t-center c-fff ${styles.txt}`}>网站设定</span>
+        </div>
+        <div className={`${styles.accountBtnOrange} cursor-p bg-orange flex align-items-center`}>
+          <Icon type="logout" style={{color: '#fff', fontSize: '18px', margin: '0 15px'}}/>
+          <span className={`flex-1 f-bolder t-center c-fff ${styles.txt}`}>登出</span>
+        </div>
+      </div>
+    </div>
+  </div>
+)
 class Header extends PureComponent {
   state = {
     coinArr: ['XBT', 'ADA', 'BCH', 'EOS', 'ETH', 'LTC', 'TRX', 'XRP'],
@@ -79,8 +191,8 @@ class Header extends PureComponent {
         <div className={`${styles.headerBar} flex align-items-center`}>
           <div className={styles.logo}>LOGO</div>
           <span className={styles.line}></span>
-          <div className={`${styles.links} flex-justify-between align-items-center f-bolder`}>
-            <Link to="/trade/XBTUSD">交易</Link>
+          <div className={`${styles.links} flex flex-justify-between align-items-center f-bolder`}>
+            <Link to="/trade/XBTUSD" style={{marginLeft: '6px'}}>交易</Link>
             <span className=''>•</span>
             <Link to="/trade/XBTUSD">账户</Link>
             <span className=''>•</span>
@@ -89,6 +201,45 @@ class Header extends PureComponent {
             <Link to="/trade/XBTUSD">参考</Link>
             <span className=''>•</span>
             <Link to="/trade/XBTUSD">API</Link>
+          </div>
+          <div className={`flex-1 flex flex-justify-end align-items-center f-12 ${styles.headerWidget}`}>
+            <span className={styles.line}></span>
+            <div className={`${styles.total} ${styles.widgetWrap}`}>
+              <div className='flex-justify-between flex flex-1'>
+                <span>总:</span>
+                <span>0.0000 XBT</span>
+              </div>
+              <div className='flex-justify-between flex flex-1'>
+                <span>可用:</span>
+                <span>0.0000 XBT</span>
+              </div>
+            </div>
+            <span className={styles.line}></span>
+            <Popover content={<Notification/>} placement='bottomRight'
+              trigger='click' overlayClassName='headerNotification'>
+              <div className={`cursor-p ${styles.widgetWrap}`}>
+                <Icon type="notification"/>
+              </div>
+            </Popover>
+            <span className={styles.line}></span>
+            <Popover content={<Lang/>} placement='bottomRight'
+              trigger='click' overlayClassName='headerLang'>
+              <div className={styles.widgetWrap}>
+                <img className={styles.langWrap} src="/image/zh-CN.svg" alt=""/>
+              </div>
+            </Popover>
+            <span className={styles.line}></span>
+            <div className={styles.widgetWrap}>
+              <Link to='/trade/XBTUSD'><Icon type="question-circle" /></Link>
+            </div>
+            <span className={styles.line}></span>
+            <Popover content={<Account/>} placement='bottomRight'
+              trigger='click' overlayClassName='headerLang'>
+              <div className={`${styles.widgetWrap} f-18`}>
+                zhaoxueyu@dae.org
+                <Icon type="caret-down" />
+              </div>
+            </Popover>
           </div>
         </div>
       </div>
